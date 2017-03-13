f76eb3ae043fd7df41bb2c6d75bb3fe42b84dddd
RockerHX <RockerHX@gmail.com>
2017-03-13 23:20:27 +0800

    Initial Commit

---
+++ SimulateLocation/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  SimulateLocation
+//
+//  Created by RockerHX on 2017/3/13.
+//  Copyright © 2017年 RockerHX. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
